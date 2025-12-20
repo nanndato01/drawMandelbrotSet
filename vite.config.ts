c40e81ac0ba1e0eb736dc55595cab288026e4b33
nanndato01 <nanndato01@gmail.com>
2025-12-20 16:11:00 +0900

    change outDir

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: "/drawMandelbrotSet/",
+  build: {outDir: 'docs'},
   plugins: [react(), tailwindcss()],
 })
